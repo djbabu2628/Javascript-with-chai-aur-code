ca69cbca6f87dbf091deea6d8828c23ce03453d3
My-Hospital <viveksunilchavhan@gmail.com>
2025-08-16 17:20:05 +0530

    preform some operation

--- basis_01/conversionOperation.js
+++ basis_01/conversionOperation.js
@@ -1,46 +1,72 @@
-let score = 23;
+// let score = 23;
 
-console.log(score);
-console.log(typeof score);
+// console.log(score);
+// console.log(typeof score);
 
-score = null;
-console.log(score)
-console.log(typeof (score));
+// score = null;
+// console.log(score)
+// console.log(typeof (score));
 
-let valueInNumber = Number(score);
+// let valueInNumber = Number(score);
 
-console.log(valueInNumber);
-console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
 
-valueInNumber = "123@jg";
+// valueInNumber = "123@jg";
 
-console.log(valueInNumber);
-console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
 
-//"33" => 33
-//"33abc" => NaN
-//true => 1 /false => 0
+// //"33" => 33
+// //"33abc" => NaN
+// //true => 1 /false => 0
 
 
-isLoggedIn = 1;
-isLoggedOut = 0;
+// isLoggedIn = 1;
+// isLoggedOut = 0;
 
-console.log([isLoggedIn, isLoggedOut])
+// console.log([isLoggedIn, isLoggedOut])
 
-let booleanInloggedIn = Boolean(isLoggedIn); booleanInloggedOut = Boolean(isLoggedOut)
+// let booleanInloggedIn = Boolean(isLoggedIn); booleanInloggedOut = Boolean(isLoggedOut)
 
-console.log([booleanInloggedIn, booleanInloggedOut])
+// console.log([booleanInloggedIn, booleanInloggedOut])
 
-let isName = "Vivek";
-let isAge = 19;
-let isNull = null;
-let isOwn
+// let isName = "Vivek";
+// let isAge = 19;
+// let isNull = null;
+// let isOwn
 
-console.log([isName, isAge, isNull]);
+// console.log([isName, isAge, isNull]);
 
-let isNameb = Boolean(isName);
-let isAgeb = Boolean(isAge);
-let isNullb = Boolean(isNull);
-let isOwnb = Boolean(isOwn);
+// let isNameb = Boolean(isName);
+// let isAgeb = Boolean(isAge);
+// let isNullb = Boolean(isNull);
+// let isOwnb = Boolean(isOwn);
 
-console.log([isNameb, isAgeb, isNullb, isOwnb]);
+// console.log([isNameb, isAgeb, isNullb, isOwnb]);
+
+
+
+
+//******************************************************** Operation ********************************************************
+
+
+let value = 3;
+let negVal = -value;
+console.log(negVal);
+
+
+let str1 ="Vivek", str2 = "Chavhan";
+let str3 = str1 + str2;
+console.log(str3);
+
+
+console.log("1" + 2);
+console.log(2 + 3 + "1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 3 + 4);
+
+let gameCounter = 100;
+console.log(gameCounter);
+console.log(gameCounter++);
+console.log(++gameCounter);
