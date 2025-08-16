24b805095bdc2ed47ca676dfb4a2215ceb76772d
My-Hospital <viveksunilchavhan@gmail.com>
2025-08-16 17:29:51 +0530

    comparision

---
+++ basis_01/comparision.js
@@ -0,0 +1,15 @@
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+// console.timeLog(!(null == 0));
+
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+// console.timeLog(!(undefined == 0));
+
+
+console.log("2" === 2);
